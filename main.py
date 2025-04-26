051279e14d3b437e89c82e478cd2ab2f25a06f14
SYDADSTREAM\sunil.sarda <sunil.sarda@extremereach.com>
2025-04-27 00:26:15 +0100

    Reverting Display changes.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-import os
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from api import route, health
@@ -15,30 +15,6 @@ ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://2.220.53.210:8080"
 ]
-
-
-def print_env_var(name, mask=True):
-    value = os.getenv(name)
-    if value:
-        if mask:
-            # Show only first 4 characters and mask the rest
-            display_value = value[:4] + "****"
-        else:
-            display_value = value
-        print(f"{name} is set: {display_value}")
-    else:
-        print(f"{name} is NOT set!")
-
-
-# Safe printing (masking sensitive values)
-print_env_var("OPENAI_API_KEY")
-print_env_var("SUPABASE_URL", mask=False)  # URL is not as sensitive
-print_env_var("SUPABASE_KEY")
-print_env_var("DB_HOST", mask=False)
-print_env_var("DB_NAME", mask=False)
-print_env_var("DB_USER", mask=False)
-print_env_var("DB_PASS")
-print_env_var("DB_PORT", mask=False)
 
 app.add_middleware(
     CORSMiddleware,
@@ -56,7 +32,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-# Logging middleware
 @app.middleware("http")
 async def log_requests(request: Request, call_next):
     logger.info(f"Incoming request: {request.method} {request.url}")
@@ -65,7 +40,5 @@ async def log_requests(request: Request, call_next):
     return response
 
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000)
